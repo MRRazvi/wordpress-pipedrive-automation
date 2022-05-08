7d2386f165db53a2d7c43506807ca72a8311acd3
rizimore <rizimore@gmail.com>
2022-05-09 01:39:53 +0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# WordPress Pipedrive Automation
+
+It is a simple plugin which will provide two-way syncing between wordpress user and pipedrive person, and additionally it will support woocommerce membership and subscription syncing as well.
+
+## Author
+Mubashir Rasool Razvi (@rizimore)  
+rizimore@outlook.com  
+[https://www.upwork.com/freelancers/~01ef7b2184f920ecf7](https://www.upwork.com/freelancers/~01ef7b2184f920ecf7)
---
+++ wordpress-pipedrive-automation.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       WordPress Pipedrive Automation
+ * Description:       It is a simple plugin which will provide two-way syncing between wordpress user and pipedrive person, and additionally it will support woocommerce membership and subscription syncing as well.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mubashir Rasool Razvi
+ * Author URI:        https://www.upwork.com/freelancers/~01ef7b2184f920ecf7
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       wordpress-pipedrive-automation
+ */

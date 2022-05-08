dff13ce72fa0d72d3922c6e084e9a1b6222531e0
rizimore <rizimore@gmail.com>
2022-05-09 01:45:52 +0500

    init

--- wordpress-pipedrive-automation.php
+++ wordpress-pipedrive-automation.php
@@ -1,5 +1,10 @@
 <?php
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 /**
  * Plugin Name:       WordPress Pipedrive Automation
  * Description:       It is a simple plugin which will provide two-way syncing between wordpress user and pipedrive person, and additionally it will support woocommerce membership and subscription syncing as well.
@@ -10,5 +15,14 @@
  * Author URI:        https://www.upwork.com/freelancers/~01ef7b2184f920ecf7
  * License:           GPL v3 or later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:       wordpress-pipedrive-automation
+ * Text Domain:       rizimore_wpa
  */
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$log = new Logger('rizimore_wpa');
+$log->pushHandler(new StreamHandler(__DIR__ . '/logs/app.log', Logger::INFO));
+
+require_once __DIR__ . '/src/webhooks/wordpress.php';
+require_once __DIR__ . '/src/webhooks/pipedrive.php';
